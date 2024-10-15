6d009c17573eaa3b97ccbe9112e033b0c47b3efb
cdomdev <dominguez5493.cd@gmail.com>
2024-10-15 15:06:35 -0500

    fix: sidebar and icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -249,6 +249,16 @@ export const Sidebar = ({ isAuthenticated }) => {
                 </span>
               </a>
             </li>
+            <li>
+              <a
+                href="/gestion-usuarios"
+                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <UserIcon />
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Gestionar usuarios
+                </span>
+              </a>
+            </li>
           </ul>
         </div>
       </aside>
